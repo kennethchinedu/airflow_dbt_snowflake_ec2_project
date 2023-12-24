0685b7f3354f30d4b8cc3fbe051f6e4ea6714061
Ubuntu <ubuntu@ip-172-31-29-175.eu-north-1.compute.internal>
2023-12-24 02:47:59 +0000

    added dbt dimensinal models, created models for featured house and house location views

--- dbt/real_estate/models/src/src_housing.sql
+++ dbt/real_estate/models/src/src_housing.sql
@@ -1,7 +1,7 @@
-WITH raw_housing AS(
-    SELECT * from {{ source('real_estate', 'house_listing') }}
+WITH raw_housing_listing AS(
+    SELECT * from {{ source('real_estate_db', 'HOUSE_LISTING_JSON_RAW') }}
 )
 
 SELECT * 
 FROM 
-    raw_housing
+    raw_housing_listing
