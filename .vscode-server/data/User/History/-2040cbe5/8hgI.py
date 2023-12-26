78b6e426e2e44bff47e73dfec5827c516efabf55
Kenneth Chinedu <67561307+kennethchinedu@users.noreply.github.com>
2023-12-26 23:13:48 +0100

    Delete .vscode-server directory

--- .vscode-server/data/User/History/-2040cbe5/8hgI.py
+++
@@ -1,7 +0,0 @@
-import json
-from datetime import timedelta, datetime
-
-#Loading config file
-with open('/home/ubuntu/airflow/config.json', r) as config_file:
-    api_host_key = json.lead(config_file)
-    
